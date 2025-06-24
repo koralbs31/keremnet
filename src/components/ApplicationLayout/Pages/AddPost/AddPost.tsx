d048c5db3ed7681829ecf690f53229b3e0286e22
koralbs31 <steinbergkoral@gmail.com>
2025-06-24 18:24:13 +0300

    made the add post connect with the api and add the posts

--- src/components/ApplicationLayout/Pages/AddPost/AddPost.tsx
+++ src/components/ApplicationLayout/Pages/AddPost/AddPost.tsx
@@ -1,13 +1,20 @@
 import React from 'react';
 import { TextField, Button } from '@mui/material';
 import { useForm } from '../../../../hooks/useForm';
+import axios from 'axios';
 import './AddPost.css';
 
 const AddPost: React.FC = () => {
   const { formData: post, handleChange, handleSubmit } = useForm(
     { author: '', title: '', content: '' },
-    (data) => {
-      alert(`Post added!\n\nTitle: ${data.title}\nContent: ${data.content}`);
+    async (data) => {
+      try {
+        await axios.post('http://localhost:8080/api/posts/add', data);
+        alert(`Post added!`);
+      } catch (error) {
+        console.error(error);
+        alert('Something went wrong while adding the post.');
+      }
     }
   );
 
@@ -16,6 +23,16 @@ const AddPost: React.FC = () => {
       <div className="add-post-container">
         <h2>Add New Post</h2>
         <form className="add-post-form" onSubmit={handleSubmit}>
+          <TextField
+            label="Author"
+            name="author"
+            variant="outlined"
+            fullWidth
+            margin="normal"
+            value={post.author}
+            onChange={handleChange}
+            required
+          />
           <TextField
             label="Post Title"
             name="title"
@@ -26,19 +43,17 @@ const AddPost: React.FC = () => {
             onChange={handleChange}
             required
           />
-
           <TextField
             label="Content"
             name="content"
             margin="normal"
             multiline
-            rows={2}
+            rows={4}
             fullWidth
             value={post.content}
             onChange={handleChange}
             required
           />
-
           <Button variant="contained" color="primary" type="submit">
             Submit Post
           </Button>
