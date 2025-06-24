bc4bf722272a588e38f46dfd3aa6bcb0203a59d3
koralbs31 <steinbergkoral@gmail.com>
2025-06-24 18:07:51 +0300

    client api added

--- src/components/ApplicationLayout/Pages/Homepage/PostsView/PostView.tsx
+++ src/components/ApplicationLayout/Pages/Homepage/PostsView/PostView.tsx
@@ -1,36 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './PostView.css';
 import PostType from '../../../../../Types/Post';
 import Post from '../../../../Post/Post';
+import PostModal from '../../../../../Modals/PostModal';
+import Button from '@mui/material/Button';
 
 interface Props {
-    posts: PostType[];
-    Loading?: boolean;
+  posts: PostType[];
+  Loading?: boolean;
 }
 
 const PostView: React.FC<Props> = ({ posts, Loading }) => {
-    return (
-        <div className="homepage">
-            <main className="homepage-main">
-                {Loading ? (
-                    <div className="homepage-loading">Loading posts...</div>
-                ) : (
-                    <ul className="homepage-posts">
-                        {posts.map((post, index) => (
-                            <li className="homepage-post" key={post.title + post.publishedAt + index}>
-                                <Post
-                                    title={post.title}
-                                    author={post.author}
-                                    publishedAt={post.publishedAt}
-                                    content={post.content}
-                                />
-                            </li>
-                        ))}
-                    </ul>
-                )}
-            </main>
-        </div>
-    );
+  const [modalOpen, setModalOpen] = useState(false);
+  const [selectedPost, setSelectedPost] = useState<PostType | null>(null);
+
+  const handleViewPost = (post: PostType) => {
+    setSelectedPost(post);
+    setModalOpen(true);
+  };
+
+  return (
+    <div className="homepage">
+      <main className="homepage-main">
+        {Loading ? (
+          <div className="homepage-loading">Loading posts...</div>
+        ) : (
+          <ul className="homepage-posts">
+            {posts.map((post, index) => (
+              <li className="homepage-post" key={post.title + post.publishedAt + index}>
+                <Post {...post} />
+                <Button variant="contained" onClick={() => handleViewPost(post)}>View Post</Button>
+              </li>
+            ))}
+          </ul>
+        )}
+      </main>
+
+      {modalOpen && selectedPost && (
+        <PostModal setOpenModal={setModalOpen} post={selectedPost} />
+      )}
+    </div>
+  );
 };
 
 export default PostView;
--- src/components/Post/Post.tsx
+++ src/components/Post/Post.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Card, CardContent, CardHeader, Typography, IconButton, Box} from '@mui/material';
 import ThumbUpAltIcon from '@mui/icons-material/ThumbUpAlt';
 import Comments from '../Comments/Comments';
